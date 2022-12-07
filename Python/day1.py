9ca3da9af4ced5973b7657d78cca96a8df345a39
jacobgabbert <jacobgabbert3@gmail.com>
2022-12-07 17:28:43 -0600

    Added Python directory, added python bootcamp day1 file

---
+++ Python/day1.py
@@ -0,0 +1,44 @@
+# use a 'hashtag' to create comments
+# print("Hello World!")
+
+# print("Day 1 - Python Print Function")
+# print("The Function is declared like this:")
+# print("print('what to print')")
+
+# use '\n' to create new lines
+# print("Hello World!\nHello World!")
+
+# use '+' to concatenate strings
+# print("Hello " + "World" + "!")
+
+# print("Day 1 - String Manipulation")
+# print('String Concatenation is done with the "+" sign.')
+# print('e.g. print("Hello " + "World!")')
+# print("New lines can be created with a backslash and n.")
+
+# use 'input()' function for input
+
+# input("What is your name?")
+# print("Hello " + input("What is your name?"))
+
+# use 'len()' function to get the length of a string
+
+# print(len(input("What is your name? ")))
+
+# print("Swapping values of variables:")
+# a = 1
+# b = 2
+# print(a, b)
+# c = a
+# a = b
+# b = c
+# print(a, b)
+
+# Day 1 - Project
+# Create a band name generator using simple variables and inputs from the user
+
+print("This is a band name generator, follow the prompts:\n")
+a = input("What city did you grow up in?\n")
+b = input("What is the name of your pet?\n")
+print("Your band name would be:", a, b)
+
