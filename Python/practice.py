cadf4e227aa398d53925399cef2f3e71f9ce463d
jacobgabbert <jacobgabbert3@gmail.com>
2022-12-07 20:16:56 -0600

    added even or add snippet to 'practice.py'

--- Python/practice.py
+++ Python/practice.py
@@ -9,8 +9,15 @@
 
 # simple while loop
 
-a = 0
-while a <= 10:
-    print(a, " ")
-    a += 1
+# a = 0
+# while a <= 10:
+#     print(a, " ")
+#     a += 1
 
+# even or odd
+
+a = int(input("Enter a number:"))
+if a % 2 == 0:
+    print("Your number,", a,"is even.")
+else:
+    print("Your number,", a,"is odd.")
