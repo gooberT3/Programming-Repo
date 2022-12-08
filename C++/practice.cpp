68d764395c65d240031a2d38d7ccf1aff217c025
jacobgabbert <jacobgabbert3@gmail.com>
2022-12-08 17:27:08 -0600

    added practice cpp file and day 2 file for python bootcamp

--- C++/practice.cpp
+++ C++/practice.cpp
@@ -4,14 +4,22 @@ class FooClass
 {
     private:
         std::string foo;
+        int num1,
+            num2;
     public:
         FooClass()
         {
             foo = "I am from a private class";
+            num1 = 1;
+            num2 = 1;
         }
         void displayFoo()
         {
             std::cout << foo;
+        }
+        void addNums()
+        {
+            std::cout << num1 + num2 << std::endl;
         }
 };
 
@@ -19,6 +27,7 @@ int main()
 {
     FooClass test;
     test.displayFoo();    
+    test.addNums();
 
     return 0;
 }
---
+++ Python/day2.py
@@ -0,0 +1,30 @@
+# prints out the nth char of the string
+# print("Hello"[4])
+
+# anything between "" is considered a string 
+# "a" "abc" "123"
+
+# twoDigitNumber = input("Type a two digit number: ")
+# print(int(twoDigitNumber[0]) + int(twoDigitNumber[1]))
+
+# height = input("Enter your height in m: ")
+# weight = input("Enter your weight in kg: ")
+# print(int(float(weight) / (float(height) ** 2)))
+
+# You have 12410 days, 1768 weeks, and 408 months left
+# 365 days | 52 weeks | 12 months
+
+# age = input("Enter your age in years: ") 
+# age = int(age)
+# ageDiff = 90 - age
+# days = 365 * ageDiff 
+# weeks = 52 * ageDiff
+# months = 12 * ageDiff
+# print(f"You have {days} days, {weeks} weeks, and {months} months left.")
+
+# Day 2 - Project
+# Bill = $150.00
+# Split between 5 people with a 12% tip
+# Format to two decimals
+
+
