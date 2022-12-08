1024af35dc0b6fe044cbc74c7569869b2f8f9fdc
jacobgabbert <jacobgabbert3@gmail.com>
2022-12-07 19:07:16 -0600

    added while loop to 'practice.py'

--- Python/practice.py
+++ Python/practice.py
@@ -1,9 +1,16 @@
 # simple if statement
 
-a = True
-b = "Hello World!"
-if a == True:
-    print(b)
-else:
-    print("Bye.")
+# a = True
+# b = "Hello World!"
+# if a == True:
+#     print(b)
+# else:
+#     print("Bye.")
+
+# simple while loop
+
+a = 0
+while a <= 10:
+    print(a, " ")
+    a += 1
 
