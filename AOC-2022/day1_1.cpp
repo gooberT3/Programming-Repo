ba1e797b1b7e09f940a2754017f185af9d04419b
jacobgabbert <jacobgabbert3@gmail.com>
2022-12-10 21:16:08 -0600

    major refactor of AOC 2022 day 1 parts 1 & 2

--- AOC-2022/day1_1.cpp
+++ AOC-2022/day1_1.cpp
@@ -1,110 +1,49 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
 
-void fileOperations(std::ifstream&, std::vector<std::string>&);
-
-void openFile(std::ifstream&);
-void closeFile(std::ifstream&);
-void readFile(std::ifstream&, std::vector<std::string>&);
-
-int dataManipulation(std::vector<std::string>&, std::vector<int>&, int);
-
-void storeIndividual(std::vector<std::string>&, std::vector<int>&);
-int findGreatest(std::vector<int>&);
-
-void display(int);
-
-int main() {
-    //object for reading data from the input file
-    std::ifstream input;
+std::vector<std::string> readFile(const std::string& fileName) {
     std::vector<std::string> fileData;
 
-    //open the file, read and store the data into a vector of strings, close the file
-    fileOperations(input, fileData);
+    std::string currentLine;
 
-    //container to store how much individial elf is carrying
-    std::vector<int> individual;
+    std::ifstream input(fileName);
 
-    //manipulate the vector read from the file, calculating each elfs amount,
-    //sorting the values from greatest to least, and find the greatest value
-    int greatest;
-    greatest = dataManipulation(fileData, individual, greatest);
-
-    //display the greatest amount of calories to the user
-    display(greatest);
-
-    return 0;
-}
-
-//manipulating the file object to open, read, and close the file
-void fileOperations(std::ifstream& input, std::vector<std::string>& fileData) {
-    openFile(input);
-    readFile(input, fileData);
-    closeFile(input);
-}
-
-//open the input file
-void openFile(std::ifstream& input) {
-    input.open("day1_input.txt");
-}
-
-//close the input file
-void closeFile(std::ifstream& input) {
-    input.close();
-}
-
-//read data from the input file into a vector of strings
-void readFile(std::ifstream& input, std::vector<std::string>& fileData) {
-    std::string tempStorage;
-
-    while(getline(input, tempStorage)) {
-        fileData.push_back(tempStorage);    
+    while(getline(input, currentLine)) {
+        fileData.push_back(currentLine);    
     }
+
+    input.close();
+
+    return fileData;
 }
 
-//manipulate the data read from the file
-int dataManipulation(std::vector<std::string>& fileData, std::vector<int>& individial, int greatest) {
-    storeIndividual(fileData, individial);
-    greatest = findGreatest(individial);
-
-    return greatest;
-}
-
-//move data from vector of strings to vector of ints for calculations
-void storeIndividual(std::vector<std::string>& fileData, std::vector<int>& individual) {
-    //variable to keep count of the current elfs calories
+std::vector<int> storeIndividual(const std::vector<std::string>& fileData) {
+    std::vector<int> individual;
     int currentCount = 0;
 
-    //loop to move through and transfer data between vectors
-    for(int i = 0; i < fileData.size(); i++) {
-        if(fileData.at(i).empty()) {
+    for(auto& element : fileData) {
+        if(element.empty()) {
             individual.push_back(currentCount);
             currentCount = 0;
         }
         else {
-            currentCount += stoi(fileData.at(i));
+            currentCount += stoi(element);
         }
     }
+    
+    return individual;
 }
 
-//find the greatest amount of calories a single elf was carrying
-int findGreatest(std::vector<int>& individual) {
-    //variable to store the greatest amount of calories
-    int greatest = 0;
+int main() {
+    std::vector<std::string> fileData = readFile("day1_input.txt");
+    std::vector<int> individual = storeIndividual(fileData);
 
-    //loop to compare all elves(elements) and see which had the largest amount of calories
-    for(int i = 0; i < individual.size(); i++) {
-        if(individual.at(i) > greatest) {
-            greatest = individual.at(i);
-        }
-    }
+    int greatest = *std::max_element(individual.begin(), individual.end());
 
-    return greatest;
-}
+    std::cout << "The greatest amount amount being carried is \"" << greatest << "\" calories.\n";
 
-//display the greatest amount of calories to the user
-void display(int greatest) {
-    std::cout << "The elf carrying the most is carrying \"" << greatest << "\" calories.";
+    return 0;
 }
 
