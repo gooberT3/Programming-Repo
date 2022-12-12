e9bf030797a02a45a9a36a440195f74a21ef40f4
jacobgabbert <jacobgabbert3@gmail.com>
2022-12-12 09:35:48 -0600

    updated practice cpp to be a singly linked list example

--- C++/practice.cpp
+++ C++/practice.cpp
@@ -1,33 +1,31 @@
 #include <iostream>
-
-class FooClass
-{
-    private:
-        std::string foo;
-        int num1,
-            num2;
-    public:
-        FooClass()
-        {
-            foo = "I am from a private class";
-            num1 = 1;
-            num2 = 1;
-        }
-        void displayFoo()
-        {
-            std::cout << foo;
-        }
-        void addNums()
-        {
-            std::cout << num1 + num2 << std::endl;
-        }
-};
-
-int main()
-{
-    FooClass test;
-    test.displayFoo();    
-    test.addNums();
-
+using namespace std;
+    struct Node {
+        int data;
+        struct Node *next;
+    };
+struct Node* head = NULL;
+    void insert(int new_data) {
+        struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+        new_node->data = new_data;
+        new_node->next = head;
+        head = new_node;
+    }
+void display() {
+    struct Node* ptr;
+    ptr = head;
+    while (ptr != NULL) {
+        cout<< ptr->data <<" ";
+        ptr = ptr->next;
+    }
+}
+int main() {
+    insert(3);
+    insert(1);
+    insert(7);
+    insert(2);
+    insert(9);
+    cout<<"The linked list is: ";
+    display();
     return 0;
 }
